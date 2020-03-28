e19695214aad4bf777c43cb3ada30b52c16e8788
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-03-28 15:29:13 +0900

    Update house_price.py

--- house_price.py
+++ house_price.py
@@ -8,6 +8,9 @@ from scipy.stats import norm, skew
 from sklearn.preprocessing import LabelEncoder
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+from  warnings import simplefilter
+from sklearn.exceptions import ConvergenceWarning
+simplefilter("ignore", category=ConvergenceWarning)
 
 pd.set_option('display.float_format', lambda x : '{:.2f}'.format(x))
 
@@ -159,5 +162,41 @@ from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 import lightgbm as lgb
 
+n_folds = 5
 
+def rmsle_cv(model):
+    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(df_train.values)
+    rmse = np.sqrt(-cross_val_score(model, df_train.values, y_train, scoring='neg_mean_squared_error', cv=kf))
+    return (rmse)
+
+lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=1))
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05, max_depth=4,
+                                   max_features='sqrt', min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state=5)
+model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, learning_rate=0.05,
+                             max_depth=3, min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571, subsample=0.5213,
+                             silent=1, random_state=7, nthread=-1)
+model_lgb = lgb.LGBMRegressor(objective='regression', num_leaves=5, learning_rate=0.05,
+                              n_estimators=720, max_bin=55, bagging_fraction=0.8,
+                              bagging_freq=5, feature_fraction=0.2319,
+                              feature_fraction_seed=9, baggging_seed=9,
+                              min_data_in_leaf=6, min_sum_hessian_in_leaf =11)
+score_lasso = rmsle_cv(lasso)
+print("\nLasso score:{:.4f} ({:.4f})\n".format(score_lasso.mean(), score_lasso.std()))
+score_ENet = rmsle_cv(ENet)
+print("ElasticNet score: {:.4f} ({:.4f})\n".format(score_ENet.mean(), score_ENet.std()))
+score_KRR = rmsle_cv(KRR)
+print("Kernel Ridge score: {:.4f} ({:.4f})\n".format(score_KRR.mean(), score_KRR.std()))
+score_xgb = rmsle_cv(model_xgb)
+print("Xgboost score: {:.4f} ({:.4f})\n".format(score_xgb.mean(), score_xgb.std()))
+score_lgb = rmsle_cv(model_lgb)
+print("LGBM score: {:.4f} ({:.4f})\n" .format(score_lgb.mean(), score_lgb.std()))
+
+models = pd.DataFrame({
+    'Model' : ['lasso', 'ENet', 'KRR','model_xgb', 'model_lgb'],
+    'Score' : ['score_lasso', 'score_ENet', 'score_KRR', 'score_xgb', 'score_lgb']})
+print(models.sort_values(by='Score', ascending=True))
 
